5444dfacaefd297579e9901ba564a7d993a371e4
porkfrnd <90595891+porkfrnd@users.noreply.github.com>
2022-04-25 15:24:33 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask
-app = Flask('app')
-
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
-
-app.run(host='0.0.0.0', port=8080)
+import time
+import random
+import string
+#print("making things ready")
+#time.sleep(1)
+#print("ready in 3")
+#time.sleep(1)
+#print("2")
+#time.sleep(1)
+#print("1")
+#time.sleep(0.5)
+#print("les go")
+#time.sleep(0.5)
+#shit = string.ascii_letters
+#s = shit + shit + shit
+while True:
+  r = random.randint(0,99999999)
+  time.sleep(0.001)
+  print("random number: " + str(r))
